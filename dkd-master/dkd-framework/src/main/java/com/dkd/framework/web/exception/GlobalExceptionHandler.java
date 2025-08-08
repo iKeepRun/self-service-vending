596484df8a244837ce5e10c7fbc6921ccb24c93e
zq <zq@czq.auto>
2025-08-08 10:08:46 +0800

    framework模块的全局异常处理类新增异常

--- dkd-master/dkd-framework/src/main/java/com/dkd/framework/web/exception/GlobalExceptionHandler.java
+++ dkd-master/dkd-framework/src/main/java/com/dkd/framework/web/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.dkd.framework.web.exception;
 import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.BindException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
@@ -141,5 +142,18 @@ public class GlobalExceptionHandler
     public AjaxResult handleDemoModeException(DemoModeException e)
     {
         return AjaxResult.error("演示模式，不允许操作");
+    }
+
+    /**
+     * 演示模式异常
+     */
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public AjaxResult handleDataIntegrityViolationException(DataIntegrityViolationException e)
+    {
+        if(e.getMessage().contains("delete")){
+
+            return AjaxResult.error("无法删除数据");
+        }
+        return AjaxResult.error("违反了数据完整性约束限制");
     }
 }
